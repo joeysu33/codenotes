b63bca7aae4bad8575b709cc88e0fbf0512ca59c
davidsu33 <davidsu33@qq.com>
2018-05-10 23:24:49 +0800

    finish findinc

--- linux/shell/findinc.sh
+++ linux/shell/findinc.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-if [ $# -ne 1 ];then
+if [ $# -lt 1 ];then
     echo "参数太少"
     exit 1
+fi
+
+inc=$1
+grep=''
+if [ $# -ge 2 ];then
+    grep=$2
 fi
 
 cc=`gcc -print-prog-name=cc1plus`
 includes=`$cc -c -v /dev/null 2>&1 | awk '{if(index($0, " /")==1) print $0;}'`
 for i in ${includes} ; do
-    find ${i} -type f -iname "*$1*"
+    if [ -z ${grep} ]; then
+        find ${i} -type f -iname "*${inc}*"
+    else
+        find ${i} -type f -iname "*${inc}*" -exec grep -inH ${grep} {} \;
+    fi
 done
+
 exit 0
