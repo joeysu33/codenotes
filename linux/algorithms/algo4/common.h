0485776b4e96c6e673ac9c5f0c2c509964adfd21
davidsu33 <davidsu33@qq.com>
2018-08-14 11:44:34 +0800

    optmize prim

---
+++ linux/algorithms/algo4/common.h
@@ -0,0 +1,24 @@
+#if !defined(_COMMON_INCLUDED_H)
+#define _COMMON_INCLUDED_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <assert.h>
+#include <stdint.h>
+#include <time.h>
+
+typedef unsigned char bool;
+
+#if !defined(true)
+#define true 1
+#endif
+
+#if !defined(false)
+#define false 0
+#endif
+
+
+#endif //_COMMON_INCLUDED_H
