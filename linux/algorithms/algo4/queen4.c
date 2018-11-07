4f681ceaf7f09555318adbf32ff18813b7f171ab
davidsu33 <davidsu33@qq.com>
2018-11-07 10:41:50 +0800

    optimize queen4

--- linux/algorithms/algo4/queen4.c
+++ linux/algorithms/algo4/queen4.c
@@ -3,11 +3,19 @@
  * 构建四个皇后，且互相不攻击
  * 同理，可以构建8皇后问题
  * 采用回溯法
+ *
+ * 回溯法的几个要素
+ * 1.递归或堆栈来实现
+ * 2.存在循环（每层循环的个数对应的是分支的个数)
+ * 3.存在判断（剪枝），成立的继续深度递归，不成立的不再深度递归
+ * 4.明确程序的入口和出口
  */ 
 
 #include <stdio.h>
+#include <assert.h>
 
-#define N 8
+#define N 10
+//#define STEP
 
 #if !defined(true)
 #define true  1
@@ -29,6 +37,10 @@ static Position q[N];
 
 bool conflict(Position *a, Position *b) {
     if(!a || !b) return false;
+    if(a->m_x >= N || b->m_x >= N) {
+        printf("Error ocurr!\n");
+        return true;
+    }
     if(a->m_x == b->m_x || a->m_y == b->m_y ||
             (a->m_x + a->m_y ==  b->m_x + b->m_y) ||
             (a->m_x - a->m_y ==  b->m_x - b->m_y))
@@ -40,91 +52,76 @@ bool conflict(Position *a, Position *b) {
 void showPlace() {
     int i,j;
     char c;
+    static int k = 0;
+    printf("--------count:%d-----------\n", ++k);
     for(i=N-1; i>=0; --i) {
         for(j=0; j<N; ++j) {
             if(j == q[i].m_x) {
                 c = '@';
             } else c = '*';
-            printf("%c ", c);
+            printf("%-2c", c);
         }
         printf("\n");
     }
     printf("\n");
     fflush(stdout);
-    //getchar();
+
+#if defined(STEP)
+    getchar();
+#endif
+}
+
+bool canPlace(int queenIndex) {
+    if(queenIndex >= N) return false;
+    if(queenIndex <= 0) return true;
+
+    Position *cur = &q[queenIndex];
+    for(int i=queenIndex-1; i>=0; --i) {
+        if(conflict(&q[i], cur)) return false;
+    }
+    return true;
 }
 
 int
 main(int argc, char *argv[]) {
-    int i, j,k, stk;
+    int i, stk;
 
     for(i=0; i<N; ++i) {
         q[i].m_x = 0;
         q[i].m_y = i;
     }
 
-    //入栈第一个皇后
-    stk = 0; //当前位置
-    showPlace();
-
-    bool s2 = false, s1 = false;
     int cnt=0;
-    while(true){
-        i = stk+1; //下一个要放的棋子
+    bool ok = false;
+    stk = 0;
+    while(true) {
+        if(stk == N) { showPlace(); cnt++; q[--stk].m_x++; }
+        if(stk < 0) { break; }
 
-        s1 = true;
-        for(; q[i].m_x<N && q[stk].m_x<N; ++q[i].m_x) {
-            s1 = false;
-            for(k=0; k<=stk; ++k) {
-                if(conflict(&q[i], &q[k])) {
-                    s1 = true;
-                    break;
-                }
-            }
-            if(!s1) break;
+        ok = false;
+        for(q[stk].m_x; q[stk].m_x<N; q[stk].m_x++) {
+            //step forward
+            if(canPlace(stk)) {
+                ok = true;
+                break;
+            } 
         }
-
-        if(s1) {
-            //回溯到上一个位置，出栈
-            q[i].m_x = 0;
-            //逐步一直进行回溯,直到q[stk].m_x != N-1,但是不能回溯到根上
-            //问题2:没有设置终止条件 stk != 0
-            /*while(q[stk].m_x == N-1 && stk != 0) {
-                q[stk].m_x = 0;
-
-                stk--;
-                if(stk < 0) {
-                    s2 = 1; //栈空,Error
-                    break;
-                } 
-            } */
-
-            //问题3:增加终止条件
-            if(stk == 0 && q[stk].m_x == N-1) { s2 = 0; printf("finished"); break; }
-
-            //对于已经发生移动的皇后，需要重新进行调整校验
-            q[stk].m_x++;
-            if(stk > 0) stk--; 
+        
+        //step backward
+        if(!ok) {
+            //恢复上次的数据
+            q[stk].m_x = 0;
+            stk--;
+            //将起始位置右移，如果溢出N则ok = false
+            if(stk >= 0)
+                q[stk].m_x++;
         } else {
-            //满足条件的皇后布局
-            if(i == N-1) {
-                printf("-----case:%d-------\n", ++cnt);
-                showPlace();
-                q[i].m_x++;
-            } else {
-                stk++;
-            }
+            stk++;
         }
     }
 
-    if(s2) 
-        printf("Not Found!\n");
-
+    printf("count=%d\n", cnt);
     return 0;
 }
-
-
-
-
 
 
