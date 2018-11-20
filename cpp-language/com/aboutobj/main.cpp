505b0522eabc98a882455097f8ccad41f12b0f76
davidsu33 <davidsu33@qq.com>
2018-11-20 17:43:10 +0800

    add aboutobject

---
+++ cpp-language/com/aboutobj/main.cpp
@@ -0,0 +1,52 @@
+/*!
+  关于虚函数是否可以内联的问题
+  1.虚函数可以inline
+  2.但是指针或引用方式调用虚接口的时候，并不依赖于inline
+*/
+#include <iostream>
+
+//#define WITH_INLINE
+#define WITH_INLINE inline
+
+using namespace std;
+
+class Base {
+public:
+    virtual ~Base() {}
+    WITH_INLINE virtual void foo() { cout << "Base::foo()"<< endl; }
+private:
+    int m_x;
+};
+
+class Derived : public Base {
+public:
+    WITH_INLINE void foo()  override { cout << "Derived::foo()" <<endl; }
+private:
+    int m_y;
+};
+
+class C1  {
+public:
+    inline void aa () { cout << "C1::aa()" << endl; }
+    void bb() ;
+};
+void C1::bb() { cout << "C1::bb()" <<endl; }
+
+int main()
+{
+    Base *b1 = new Derived();
+    b1->foo();
+
+    Derived d;
+    d.foo();
+
+    Base b2 = d;
+    b2.foo();
+
+    C1 c1;
+    c1.aa();
+    c1.bb();
+
+    return 0;
+}
+
--- cpp-language/com/chap01/main.cpp
+++ cpp-language/com/chap01/main.cpp
@@ -1,4 +1,9 @@
+/*!
+  C++对象的布局*/
 #include <iostream>
+#include <typeinfo>
+#include <cstdio>
+#include <cassert>
 
 using namespace std;
 
@@ -63,6 +68,20 @@ int main(int argc, char *argv[])
 
     Derived1 *dd1 = dynamic_cast<Derived1*>(d1);
     Derived2 *dd2 = dynamic_cast<Derived2*>(d2);
+
+    assert(sizeof(void*) == sizeof(uint64_t));
+    uint64_t *p = reinterpret_cast<uint64_t*>(dd1);
+    uint64_t addr1 = *p;
+    printf("addr1=0x%llx\n", addr1);
+    uint64_t *vtbl = reinterpret_cast<uint64_t*>(addr1);
+    for(int i=0; i<3; ++i) printf("addr[%d]=0x%llx\n", i, *(vtbl+i));
+
+//    void * vtbl = static_cast<void*>((*dd1));
+//    type_info * ti = static_cast<type_info*>(*vtbl);
+//    type_info *ti = static_cast<type_info*>(getPointerPointer(dd2));
+
+//    const type_info *ti2 = &typeid(dd1);
+//    const char *name = ti2->name();
 
     delete dd1;
     delete dd2;
