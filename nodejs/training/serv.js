d0c69e73a59d3eeb2c667a510ad8cb04e4af54db
davidsu33 <davidsu33@qq.com>
2019-05-09 21:42:53 +0800

    add nodejs

---
+++ nodejs/training/serv.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end('Hello nodejs.\n');
+}).listen(8888);
+
+console.log('Server running at http://127.0.0.1:8888');
